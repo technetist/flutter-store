0fc2a6e4252ca962a67f0045f97a77e5d34a6e46
Adrien Maranville <Adrien@Maranville.io>
2020-08-22 21:07:13 -0600

    adds product detail view

---
+++ lib/pages/product_detail_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_store/models/product.dart';
+import 'package:flutter_store/pages/products_page.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  final Product item;
+
+  ProductDetailPage({this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    final String pictureUrl = 'http://localhost:1337/${item.picture['url']}';
+    final Orientation orientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item.name),
+      ),
+      body: Container(
+        decoration: gradientBackground,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Hero(
+                tag: item,
+                child: Image.network(
+                  pictureUrl,
+                  width: orientation == Orientation.portrait ? 600 : 250,
+                  height: orientation == Orientation.portrait ? 400 : 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Text(
+              item.name,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              '\$${item.price}',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+            Flexible(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(left: 32.0, right: 32.0, bottom: 32.0),
+                  child: Text(item.description),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_store/Models/app_state.dart';
 import 'package:flutter_store/models/product.dart';
+import 'package:flutter_store/pages/product_detail_page.dart';
 
 class ProductItem extends StatelessWidget {
   final Product item;
@@ -11,35 +12,47 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String pictureUrl = 'http://localhost:1337/${item.picture['url']}';
-    return GridTile(
-      child: Image.network(
-        pictureUrl,
-        fit: BoxFit.cover,
+    return InkWell(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return ProductDetailPage(item: item);
+          }
+        )
       ),
-      footer: GridTileBar(
-        title: FittedBox(
-            fit: BoxFit.scaleDown,
-            alignment: Alignment.centerLeft,
-            child: Text(
-              item.name,
-              style: TextStyle(fontSize: 20.0),
-            )),
-        subtitle: Text(
-          '\$${item.price}',
-          style: TextStyle(fontSize: 16.0),
+      child: GridTile(
+        child: Hero(
+          tag: item,
+          child: Image.network(
+            pictureUrl,
+            fit: BoxFit.cover,
+          ),
         ),
-        backgroundColor: Color(0xBB000000),
-        trailing: StoreConnector<AppState, AppState>(
-            converter: (store) => store.state,
-            builder: (_, state) {
-              return state.user != null
-                  ? IconButton(
-                      icon: Icon(Icons.shopping_cart),
-                      color: Colors.white,
-                      onPressed: () => print('clicked'),
-                    )
-                  : Text('');
-            }),
+        footer: GridTileBar(
+          title: FittedBox(
+              fit: BoxFit.scaleDown,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                item.name,
+                style: TextStyle(fontSize: 20.0),
+              )),
+          subtitle: Text(
+            '\$${item.price}',
+            style: TextStyle(fontSize: 16.0),
+          ),
+          backgroundColor: Color(0xBB000000),
+          trailing: StoreConnector<AppState, AppState>(
+              converter: (store) => store.state,
+              builder: (_, state) {
+                return state.user != null
+                    ? IconButton(
+                        icon: Icon(Icons.shopping_cart),
+                        color: Colors.white,
+                        onPressed: () => print('clicked'),
+                      )
+                    : Text('');
+              }),
+        ),
       ),
     );
   }
